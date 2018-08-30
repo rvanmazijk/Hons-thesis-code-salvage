5e99717d55d67af6fc6564b918ebe7f81941c730
Ruan van Mazijk <ruanvmazijk@gmail.com>
2018-08-30 16:22:46 +0200

    Add Scripts version

---
+++ BRT-code-salvage/helper-funs/map_to_z_et_al.R
@@ -0,0 +1,11 @@
+map_0_to_1 <- function(x) {
+    (x - min(x)) /
+    (max(x) + min(x))
+}
+map_neg1_to_1 <- function(x) {
+    2 * ((x - min(x)) / (max(x) + min(x))) - 1
+}
+
+map_to_z <- function(x) {
+    (x - mean(x)) / sd(x)
+}
